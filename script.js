b4e46e27338f242a44dfeecd29c2a03d82bc96d5
shivamicy1-stack <shivamicy1@gmail.com>
2025-09-05 19:55:45 +0530

    Add files via upload
    Initial commit. Adding full website files.

---
+++ script.js
@@ -0,0 +1,14 @@
+// Mobile Menu Toggle
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+// Close menu when a link is clicked
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
